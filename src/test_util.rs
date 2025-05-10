1ec6d926baab341d6f56e6e2e5f0cf72a7967330
Mike Heffner <mikeh@fesnel.com>
2025-05-10 12:26:40 -0400

    Fix test includes

--- src/test_util.rs
+++ src/test_util.rs
@@ -1,9 +1,6 @@
 use std::sync::Once;
 
-// used for testing
-#[allow(dead_code)]
 static INIT_CRYPTO: Once = Once::new();
-#[allow(dead_code)]
 pub fn init_crypto() {
     INIT_CRYPTO.call_once(|| {
         rustls::crypto::aws_lc_rs::default_provider()
